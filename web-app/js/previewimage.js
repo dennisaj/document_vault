e245cc983ae7ccad95c87eccbcb8534d8e74a32b
Seth Miller <smiller@paperlesstech.us>
2011-07-14 15:32:44 -0400

    Added spinner when loading preview image popup
    Change-Id: I39a46f6d84256238733cb8f74b1ed41fbe3cfe53

--- web-app/js/previewimage.js
+++ web-app/js/previewimage.js
@@ -2,29 +2,30 @@ var PreviewImage = {
 	show: function(uri) {
 		$('body').append('<div id="preview"></div>');
 		var $imageDialog = $('#preview');
+
+		var $clone = $('#spinner').clone().attr('id', 'spinner-clone').css('display', 'block').width(600).height(600);
+		$imageDialog.append($clone);
+
+		$imageDialog.dialog({
+			close: function(event, ui) {
+				$imageDialog.remove();
+			},
+			modal: true,
+			open: function(event, ui) {
+				$('.ui-widget-overlay').click(function() {
+					$imageDialog.dialog('close');
+				});
+			},
+			position: 'center',
+			resizable: false,
+			width: 'auto'
+		});
+
 		var image = new Image();
 		image.src = uri;
 
 		var load = function() {
-			$imageDialog.append($(image).width(Math.min(600, image.width)));
-
-			$imageDialog.dialog({
-				autoOpen: false,
-				close: function(event, ui) {
-					$imageDialog.remove();
-				},
-				modal: true,
-				open: function(event, ui) {
-					$('.ui-widget-overlay').click(function() {
-						$imageDialog.dialog('close');
-					});
-				},
-				resizable: false,
-				width: 'auto'
-			});
-
-			// Chrome won't center the dialog correctly without this tiny waiting period
-			setTimeout(function() {$imageDialog.dialog('open')}, 1);
+			$imageDialog.html($(image).width(Math.min(600, image.width))).dialog('option', 'position', 'center');
 		};
 
 		if (image.complete) {
